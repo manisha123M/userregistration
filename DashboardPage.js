222df172365d3c74ab294f1cfd4504c9841fe6dd
MANISHA <153983657+manisha123M@users.noreply.github.com>
2025-05-07 16:09:12 +0530

    Update DashboardPage.js

--- DashboardPage.js
+++ DashboardPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
 import Header from './Header';
@@ -6,10 +6,14 @@ import Footer from './Footer';
 import AnimatedBackground from './AnimatedBackground'; 
 
 function Dashboard() {
+  const[showVideo,setShowVideo]=useState(false);
   const navigate = useNavigate();
 
   const handleButtonClick = (route) => {
-    navigate(route);
+    if(route=== '/nursery'){
+      setShowVideo(true);
+    }else{
+    navigate(route);}
   };
 
   return (
@@ -17,10 +21,28 @@ function Dashboard() {
       <AnimatedBackground /> 
       <Header />
       <main className="dashboard-main">
-        <button onClick={() => handleButtonClick('/nursery')}>3-5 years</button>
-        <button onClick={() => handleButtonClick('/primary')}>6-11 years</button>
-        <button onClick={() => handleButtonClick('/secondary')}>13-15 years</button>
-      </main>
+        <div className="button">
+      <h2><b>Choose the domain you want</b></h2>
+  <div className="button-container">
+    <button onClick={() => handleButtonClick('/nursery')}>3-5 years</button>
+    <button onClick={() => handleButtonClick('/primary')}>6-11 years</button>
+    <button onClick={() => handleButtonClick('/secondary')}>13-15 years</button>
+  </div></div>
+  {showVideo && (
+  <div className="video-container">
+    <iframe
+      width="640"
+      height="360"
+      src="https://www.youtube.com/embed/p3AdDBsR9Os?autoplay=1"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </div>
+)}
+
+</main>
+
       <Footer />
     </div>
   );
