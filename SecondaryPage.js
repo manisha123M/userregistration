91655d85ee1d72c74d3b05f1a2053a7c39a7da22
MANISHA <153983657+manisha123M@users.noreply.github.com>
2025-05-08 16:20:53 +0530

    Update SecondaryPage.js

--- SecondaryPage.js
+++ SecondaryPage.js
@@ -68,7 +68,7 @@ useEffect(() => {
     <div className="primary-page">
       <Header />
       <main className="content">
-        <h1>Primary Classes (1 - 6)</h1>
+        <h1> Classes 7th standard to 8th Standard</h1>
 
         <div className="video-section">
           <h2>Lesson {currentVideoIndex + 1}</h2>
